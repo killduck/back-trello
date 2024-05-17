8f078e9532a1bc4b57b0e4fec6210e6d0dbae67c
Ilya616 <ipoletuev@mail.ru>
2024-05-17 19:56:46 +0300

    Добавил базу postgres

--- trello/trello/views.py
+++ trello/trello/views.py
@@ -1,6 +1,42 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET', 'POST'])
+
+@api_view(["GET", "POST", "PUT"])
 def test(request):
-    return Response('Ура получилось')
+    return Response(
+        [
+            {
+                "id": 1,
+                "name": "backlog",
+                "order": 1,
+                "cards": [
+                    {
+                        "id": 1,
+                        "name": "Максим es lint",
+                        "author_id": 3,
+                        "order": 1,
+                    },
+                    {
+                        "id": 2,
+                        "name": "Лёня хреначит реакт компоненты",
+                        "author_id": 2,
+                        "order": 2,
+                    },
+                ],
+            },
+            {
+                "id": 2,
+                "name": "in progress",
+                "order": 2,
+                "cards": [
+                    {
+                        "id": 3,
+                        "name": "Кнопки меню",
+                        "author_id": 4,
+                        "order": 1,
+                    }
+                ],
+            },
+        ]
+    )
