35649d2c9853f4772422d9dd7d4bf242f30e9e62
Leo_Kildushev <kildushev@gmail.com>
2024-05-30 01:10:30 +0300

    перестановка колонок, добавление новой записываются в БД

--- trello/trello/urls.py
+++ trello/trello/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("columns/", views.columns, name="columns"),
     path("columns/edite/", views.columns_edite, name="columns-edite"),
     path("cards/", views.cards, name="cards"),
-    path('test/', views.test, name='test'),
+    # path('test/', views.test, name='test'),
     path('admin/', admin.site.urls),
 ]
