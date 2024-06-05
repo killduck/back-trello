7928417c0d12344b2e0d5e9f1e5501e57b98b721
Leo_Kildushev <kildushev@gmail.com>
2024-06-05 15:15:13 +0300

    почистил и поправил "view.py", вынес функции из "view.py" в column_functions, поправил "urls.py"

--- trello/trello/views.py
+++ trello/trello/views.py
@@ -4,151 +4,30 @@ from rest_framework.response import Response
 
 from .models import Card, Column, Person
 from .serializers import CardSerializer, ColumnSerializer, PersonSerializer
-
-
-# @api_view(["GET", "POST", "PUT"])
-# def test(request):
-#     return Response(
-#         [
-#             {
-#                 "id": 1,
-#                 "name": "backlog",
-#                 "order": 1,
-#                 "cards": [
-#                     {
-#                         "id": 1,
-#                         "name": "Максим es lint",
-#                         "author_id": 3,
-#                         "order": 1,
-#                     },
-#                     {
-#                         "id": 2,
-#                         "name": "Лёня хреначит реакт компоненты",
-#                         "author_id": 2,
-#                         "order": 2,
-#                     },
-#                 ],
-#             },
-#             {
-#                 "id": 2,
-#                 "name": "in progress",
-#                 "order": 2,
-#                 "cards": [
-#                     {
-#                         "id": 3,
-#                         "name": "Кнопки меню",
-#                         "author_id": 4,
-#                         "order": 1,
-#                     }
-#                 ],
-#             },
-#         ]
-#     )
-
-
-def change_order_columns(request, columns_db):
-    # очистка в таблице Column полей "order" для перезаписи
-    for column_db in columns_db:
-        column_db.order = None
-        column_db.save(update_fields=["order"])
-
-    for column in request.data:
-        for column_db in columns_db:
-            if column_db.id == column["id"] and column_db.name == column["name"]:
-                column_db.order = column["order"]
-                column_db.save(update_fields=["order"])
-
-    # создаём новые порядковые номера для колонок
-    # serial = 0
-    # for column in request.data:
-    #     column["order"] = serial
-    #     try:
-    #         # перезаписываем в БД для соответствующих колонок поля "order"
-    #         for column_db in columns_db:
-    #             if column_db.id == column["id"] and column_db.name == column["name"]:
-    #                 column_db.order = column["order"]
-    #                 column_db.save(update_fields=["order"])
-    #     except Exception as error:
-    #         print(f"{error} _Нет такой колонки;")
-    #     serial += 1
-
-
-def create_new_column(request, columns_db):
-    ids = []
-    orders = []
-    new_data = {}
-    # собираем все "id" и "order" из DB
-    for column_db in columns_db:
-        ids.append(column_db.id)
-        orders.append(column_db.order)
-    # записываем новые данные для новой колонки
-    for column_req in request.data:
-        if column_req["id"] not in ids:
-            new_data = {
-                "id": max(ids) + 1,
-                "name": column_req["name"],
-                "order": max(orders) + 1,
-            }
-    return new_data
+from .views_functions.column_functions import change_order_columns, check_new_column
 
 
 @api_view(["GET"])
 def columns(request):
-
-    if request.method == "POST":
-        columns_db = Column.objects.all()
-        # print(len(columns_db))
-        # print(len(request.data))
-        print(request.data)
-        if len(columns_db) == len(request.data):
-            change_order_columns(request, columns_db)
-            print("обновили порядок колонок в БД")
-
-        elif len(columns_db) > len(request.data):
-            print("была удаленна колонка и нужно обновить БД")
-
-        elif len(columns_db) < len(request.data):
-            # проверяем и готовим данные для колонки перед записью в БД
-            new_data = create_new_column(request, columns_db)
-            # добавляем колонку в БД
-            Column.objects.create(
-                id=new_data["id"],
-                name=new_data["name"],
-                order=new_data["order"],
-            )
-            print("была добавлена колонка и нужно обновить БД")
-
-        else:
-            print("если что-то сюда прилетит, то будем разбираться")
-
     queryset = Column.objects.all()
     serializer = ColumnSerializer(queryset, many=True)
     return Response(serializer.data)
 
 
 @api_view(["POST"])
-def edite_columns(request):
+def create_columns(request):
     columns_db = Column.objects.all()
 
-    print(request.data)
-    if len(columns_db) == len(request.data):
-        change_order_columns(request, columns_db)
-        print("обновили порядок колонок в БД")
-
-    elif len(columns_db) > len(request.data):
-        print("была удаленна колонка и нужно обновить БД")
-
-    elif len(columns_db) < len(request.data):
+    if len(columns_db) < len(request.data):
         # проверяем и готовим данные для колонки перед записью в БД
-        new_data = create_new_column(request, columns_db)
+        new_data = check_new_column(request, columns_db)
         # добавляем колонку в БД
         Column.objects.create(
             id=new_data["id"],
             name=new_data["name"],
             order=new_data["order"],
         )
-        print("была добавлена колонка и нужно обновить БД")
-
+        print("добавлена колонка в БД")
     else:
         print("если что-то сюда прилетит, то будем разбираться")
 
@@ -157,32 +36,52 @@ def edite_columns(request):
     return Response(serializer.data)
 
 
-@api_view(
-    [
-        "GET",
-        "POST",
-    ]
-)
-def columns_edite(request):
-    if request.method == "POST":
-        serializer = ColumnSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(["POST"])
+def swap_columns(request):
+    columns_db = Column.objects.all()
+    # print(request.data)
+    if len(columns_db) == len(request.data):
+        change_order_columns(request, columns_db)
+        print("обновили порядок колонок в БД")
+    else:
+        print("если что-то сюда прилетит, то будем разбираться")
+
     queryset = Column.objects.all()
-    print(queryset)
     serializer = ColumnSerializer(queryset, many=True)
-    print(serializer)
     return Response(serializer.data)
 
 
-@api_view(
-    [
-        "GET",
-        "POST",
-    ]
-)
+@api_view(["POST"])
+def delete_columns(request):
+    columns_db = Column.objects.all()
+
+    if len(columns_db) > len(request.data):
+        # тут нужен код для удаления колонки !!!
+        print("была удаленна колонка и нужно обновить БД")
+    else:
+        print("если что-то сюда прилетит, то будем разбираться")
+
+    queryset = Column.objects.all()
+    serializer = ColumnSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+
+# @api_view(["GET", "POST",])
+# def columns_edite(request):
+#     if request.method == "POST":
+#         serializer = ColumnSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     queryset = Column.objects.all()
+#     print(queryset)
+#     serializer = ColumnSerializer(queryset, many=True)
+#     print(serializer)
+#     return Response(serializer.data)
+
+
+@api_view(["GET", "POST",])
 def cards(request):
     if request.method == "POST":
         serializer = CardSerializer(data=request.data)
---
+++ trello/trello/views_functions/column_functions.py
@@ -0,0 +1,43 @@
+
+def change_order_columns(request, columns_db):
+    # очистка в таблице Column полей "order" для перезаписи
+    for column_db in columns_db:
+        column_db.order = None
+        column_db.save(update_fields=["order"])
+
+    # перезаписываем в БД для соответствующих колонок поля "order"
+    for column in request.data:
+        for column_db in columns_db:
+            if column_db.id == column["id"] and column_db.name == column["name"]:
+                column_db.order = column["order"]
+                column_db.save(update_fields=["order"])
+
+
+def check_new_column(request, columns_db):
+    ids = []
+    orders = []
+    new_data = {}
+    # собираем все "id" и "order" из DB
+    if len(columns_db) > 0:
+        for column_db in columns_db:
+            ids.append(column_db.id)
+            orders.append(column_db.order)
+        # записываем новые данные для новой колонки
+        for column_req in request.data:
+            if ((column_req["id"] not in ids) and
+                    ((max(ids)+1) == column_req["id"]) and
+                    ((max(orders)+1) == column_req["order"])):
+                new_data = {
+                    "id": max(ids) + 1,
+                    "name": column_req["name"],
+                    "order": max(orders) + 1,
+                }
+    else:
+        for column_req in request.data:
+            if column_req["id"] == 1 and column_req["order"] == 1:
+                new_data = {
+                    "id": column_req["id"],
+                    "name": column_req["name"],
+                    "order": column_req["order"],
+                }
+    return new_data
