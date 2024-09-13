dcbacb5fb8849c9dc42e16eaa5661dbdee4a6d01
Andrey Rubtsov <90990985+Raa78@users.noreply.github.com>
2024-09-13 10:38:53 +0300

    Правка для запуска Celery на сервере

--- trello/trello/celery.py
+++ trello/trello/celery.py
@@ -4,7 +4,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'trello.settings')
 
-app = Celery('<name_of_project>')
+app = Celery('trello')
 app.config_from_object('django.conf:settings', namespace="CELERY")
 app.autodiscover_tasks()
 
